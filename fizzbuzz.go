2e1caf73280c4bcd6d2f8d01737403aa8b783325
mimikun <karaage_nennen@icloud.com>
2019-03-01 19:46:32 +0900

    update

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -3,13 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("3の倍数のときfizz, 4の倍数のときbuzz, 12の倍数のときfizzbuzz")
 	for i := 1; i < 20; i++ {
-		if i%3 == 0 {
-			fmt.Println("fizz")
+		if i%12 == 0 {
+			fmt.Println("fizzbuzz")
 		} else if i%4 == 0 {
 			fmt.Println("buzz")
-		} else if i%12 == 0 {
-			fmt.Println("fizzbuzz")
+		} else if i%3 == 0 {
+			fmt.Println("fizz")
 		} else {
 			fmt.Println(i)
 		}
